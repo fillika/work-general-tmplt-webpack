64f7a36c5549fdf5924bae8875cc439c09ac1f4d
Aleksandr Mironenko <alfie.mir@gmail.com>
2020-06-10 19:20:01 +0300

    Проверил работоспособность картинок. Добавил правил в eslint. Добавил gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -11,117 +11,117 @@ const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
 
 const optimization = () => {
-    const config = {};
+  const config = {};
 
-    if (isProd) {
-        config.minimizer = [
-            new OptimizeCssAssetsPlugin(),
-            new TerserPlugin(),
-        ];
-    }
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+    ];
+  }
 
-    return config;
+  return config;
 };
 
 module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'project.min.js',
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 4100,
-    },
-    optimization: optimization(),
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].min.css',
-        }),
-        new CleanWebpackPlugin(),
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                postcss: [autoprefixer()],
-            },
-        }),
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            minify: false,
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {},
-                    },
-                    'css-loader',
-                    'postcss-loader',
-                ],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {},
-                    },
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|webp|svg|webp)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[folder]/[name].[ext]',
-                        },
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 80,
-                            }, // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: [
-                                    0.9,
-                                    1,
-                                ],
-                                speed: 10,
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            }, // the webp option will enable WEBP
-                            webp: {
-                                quality: 75,
-                            },
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(ttf|woff|woff2|eot|)$/i,
-                loader: 'file-loader',
-            },
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'project.min.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4100,
+  },
+  optimization: optimization(),
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].min.css',
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [autoprefixer()],
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+          'postcss-loader',
         ],
-    },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp|svg|)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[folder]/[name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 80,
+              }, // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [
+                  0.9,
+                  1,
+                ],
+                speed: 10,
+              },
+              gifsicle: {
+                interlaced: false,
+              }, // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|)$/i,
+        loader: 'file-loader',
+      },
+    ],
+  },
 };
