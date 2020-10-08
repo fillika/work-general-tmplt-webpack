75aab738fab78720904727956dcbb00bfd7c82c5
Aleksandr Mironenko <alfie.mir@gmail.com>
2020-10-08 20:23:12 +0300

    ОТклÑтключил импорт реÐактÐа

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import '@scripts/js/app';
 import '@scripts/ts/index.ts';
-import '@scripts/tsx/index';
+// import '@scripts/tsx/index'; // Если надо подключить реакт
 import '@styles/scss/index.scss';
 import '@styles/css/style.css';
